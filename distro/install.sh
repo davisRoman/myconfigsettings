0156e30ea544aabb7c6927c5228472972b40ed56
davis roman <davis.roman84@gmail.com>
2019-02-24 06:10:34 +0000

    install lsof

--- distro/install.sh
+++ distro/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for package in minicom xterm tmux tig
+for package in minicom xterm tmux tig lsof
 do
     if ! dpkg -l | grep -q $package;then
         echo "$package needs to be installed"
