c634486b8818ad4ce0c3880e4a34e4451d7c221c
davisRoman <davis.roman84@gmail.com>
2019-02-24 02:10:58 -0500

    distro related packages should be installed first

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 set -e
 
+echo ">>>> Customizing debian-based distro <<<<"
+echo ""
+pushd distro
+   ./install.sh
+popd
+
 echo ">>>> Customizing vim <<<<"
 echo ""
 pushd vim
    ./install.sh
 popd
 
-echo ">>>> Customizing debian-based distro <<<<"
-echo ""
-pushd distro
-   ./install.sh
-popd
+
