829bc721947ff8beba747ffc4b332e518a640a39
davis roman <davis.roman84@gmail.com>
2019-02-24 06:07:00 +0000

    automatically select yes when installing packages

--- distro/install.sh
+++ distro/install.sh
@@ -6,7 +6,7 @@ for package in minicom xterm tmux cscope exuberant-ctags
 do
     if ! dpkg -l | grep -q $package;then
         echo "$package needs to be installed"
-        sudo apt-get install $package
+        sudo apt-get -y install $package
     else
         echo "$package already installed!"
     fi
