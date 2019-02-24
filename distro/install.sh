e5aa053288dcfa5327496957c7cbd456c6968b54
davisRoman <davis.roman84@gmail.com>
2019-02-24 02:30:55 -0500

    install g++

--- distro/install.sh
+++ distro/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for package in minicom xterm tmux tig lsof curl
+for package in minicom xterm tmux tig lsof curl g++
 do
     if ! dpkg -l | grep -q $package;then
         echo "$package needs to be installed"
