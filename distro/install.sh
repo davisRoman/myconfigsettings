f0ca3c3b001d3d0fa40aeb76dd148fdc35722446
davis roman <davis.roman84@gmail.com>
2019-02-24 22:50:37 -0500

    install screenkey

--- distro/install.sh
+++ distro/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for package in git minicom xterm tmux tig lsof curl g++ htop silversearcher-ag meld
+for package in git minicom xterm tmux tig lsof curl g++ htop silversearcher-ag meld screenkey
 do
     if ! dpkg -l | grep -q $package;then
         echo "$package needs to be installed"
