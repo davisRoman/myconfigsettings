982c4b8b31a14205fb88d070d86e6bd49d0c8821
davis roman <davis.roman84@gmail.com>
2019-02-24 22:20:40 -0500

    "Better to ask for forgiveness than to ask for fermission" - G.H.

--- vim/install.sh
+++ vim/install.sh
@@ -7,24 +7,6 @@ do
    ! dpkg -l | grep -q $package && sudo apt-get -y install $package || echo "$package already installed!"
 done
 
-if [ -d ~/.vim/ ] || [ -f ~/.viminfo ] || [ -f ~/.vimrc ]; then
-    while true; do
-        read -p "Existing vim related files found in your home directory. Should I clobber (Y/n)?" yn
-        case $yn in
-            [Nn]* ) echo "";
-                    echo "Please back up files and try again";
-                    exit
-                    ;;
-            [Yy]* ) break
-                    ;;
-            * ) echo "Please answer yes or no."
-                    ;;
-        esac
-    done
-fi
-
-rm -rf ~/.vim*
-
 ln -sf $PWD/vimrc ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
