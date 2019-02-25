3630f470f156f8fa23d782de8d8f824884c88f1b
davis roman <davis.roman84@gmail.com>
2019-02-24 22:29:53 -0500

    download & install fzf

--- distro/install.sh
+++ distro/install.sh
@@ -12,3 +12,10 @@ do
     fi
 done
 
+if ! [ -f ~/.fzf/install-complete ];then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    pushd ~/.fzf
+        ./install --all
+        touch install-complete
+    popd
+fi
