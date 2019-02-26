190a38c5ae00ca27383e3c54a5c1dc694436af04
davis roman <davis.roman84@gmail.com>
2019-02-25 23:36:07 -0500

    add figlet package

--- distro/install.sh
+++ distro/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for package in git minicom xterm tmux tig lsof curl g++ htop silversearcher-ag meld screenkey libbsd-dev youtube-dl
+for package in git minicom xterm tmux tig lsof curl g++ htop silversearcher-ag meld screenkey libbsd-dev youtube-dl figlet
 do
     if ! dpkg -l | grep -q $package;then
         echo "$package needs to be installed"
